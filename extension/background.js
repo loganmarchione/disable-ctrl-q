ee35de66e1bbffd8a436417ce528c93f329af486
Logan Marchione <loganmarchione@users.noreply.github.com>
2017-09-03 21:00:05 -0400

    Update background.js

--- extension/background.js
+++ extension/background.js
@@ -1,7 +1,3 @@
-browser.commands.onCommand.addListener(function(command) {
-  if (command == "toggle") {
-    var myAudio = new Audio();
-    myAudio.src = "sound.ogg";
-    myAudio.play();
-  }
+browser.commands.onCommand.addListener(function(toggle) {
+    new Audio("sound.ogg").play();
 });
