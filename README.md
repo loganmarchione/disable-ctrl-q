782be0fb9d8b9faa381d0339b293ca8a5ebefd3a
Logan Marchione <logan@loganmarchione.com>
2021-03-03 12:49:10 -0500

    Update to add self-uninstall

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# This extension is no longer maintained.
+# This extension is no longer maintained. The master branch of this extension will do a self-uninstall. For a working copy, use an older release.
+
+⚠️ After 21 years, Mozilla finally fixed bug https://bugzilla.mozilla.org/show_bug.cgi?id=52821. ⚠️
+
+⚠️ Starting in FF87, you can set `browser.quitShortcut.disabled` = `true` in `about:config` to prevent Firefox from closing when pressing Ctrl+Q on all platforms (including Linux). ⚠️
 
 # Disable Ctrl+Q
 WebExtension that disables Ctrl+Q in Firefox  
--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,14 @@
 browser.commands.onCommand.addListener(function(toggle) {
     console.log("Ctrl+Q pressed!")
 });
+
+function onCanceled(error) {
+    console.log(`Canceled: ${error}`);
+  }
+
+  var uninstalling = browser.management.uninstallSelf({
+    showConfirmDialog: true,
+    dialogMessage: "After 21 years, Mozilla finally fixed bug https://bugzilla.mozilla.org/show_bug.cgi?id=52821. Starting in FF87, you can set `browser.quitShortcut.disabled` = `true` in `about:config` to prevent Firefox from closing when pressing Ctrl+Q on all platforms (including Linux).\nI'm retiring this extension. The extension will now attempt a self-uninstall.\n"
+  });
+
+  uninstalling.then(null, onCanceled);
